c28d642509fc15c779fb3363692423db6215c76a
WIFI连接超时 <localaccount@qq.com>
2025-10-21 10:41:37 +0800

    feat(mini-program): 初始化小程序工程结构

---
+++ mini-program/config/index.ts
@@ -0,0 +1,123 @@
+import type { UserConfigExport } from '@tarojs/cli'
+import process from 'node:process'
+import NutUIResolver from '@nutui/auto-import-resolver'
+import { defineConfig } from '@tarojs/cli'
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
+import Components from 'unplugin-vue-components/webpack'
+import devConfig from './dev'
+import prodConfig from './prod'
+
+// https://taro-docs.jd.com/docs/next/config#defineconfig-辅助函数
+export default defineConfig<'webpack5'>(async (merge, { command: _command, mode: _mode }) => {
+  const baseConfig: UserConfigExport<'webpack5'> = {
+    projectName: 'mini-program',
+    date: '2025-10-20',
+    designWidth(input) {
+      // 确保 input 是对象且含有 file 属性时才进行判断
+      if (typeof input === 'object' && input !== null && 'file' in input) {
+        if (input.file?.replace(/\\+/g, '/').indexOf('@nutui') > -1) {
+          return 375
+        }
+      }
+      return 750
+    },
+    deviceRatio: {
+      640: 2.34 / 2,
+      750: 1,
+      375: 2,
+      828: 1.81 / 2,
+    },
+    sourceRoot: 'src',
+    outputRoot: 'dist',
+    plugins: ['@tarojs/plugin-html'],
+    defineConstants: {
+    },
+    copy: {
+      patterns: [
+      ],
+      options: {
+      },
+    },
+    framework: 'vue3',
+    compiler: {
+      type: 'webpack5',
+      prebundle: {
+        enable: false,
+      },
+    },
+    cache: {
+      enable: true, // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+    },
+    mini: {
+      postcss: {
+        pxtransform: {
+          enable: true,
+          config: {
+
+          },
+        },
+        cssModules: {
+          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+          config: {
+            namingPattern: 'module', // 转换模式，取值为 global/module
+            generateScopedName: '[name]__[local]___[hash:base64:5]',
+          },
+        },
+      },
+      webpackChain(chain) {
+        chain.resolve.plugin('tsconfig-paths').use(TsconfigPathsPlugin)
+        chain.plugin('unplugin-vue-components').use(Components({
+          resolvers: [NutUIResolver({ taro: true })],
+        }))
+      },
+      // @ts-expect-error 微信配置组件懒加载
+      lazyCodeLoading: 'requiredComponents',
+    },
+    h5: {
+      publicPath: '/',
+      staticDirectory: 'static',
+      output: {
+        filename: 'js/[name].[hash:8].js',
+        chunkFilename: 'js/[name].[chunkhash:8].js',
+      },
+      miniCssExtractPluginOption: {
+        ignoreOrder: true,
+        filename: 'css/[name].[hash].css',
+        chunkFilename: 'css/[name].[chunkhash].css',
+      },
+      postcss: {
+        autoprefixer: {
+          enable: true,
+          config: {},
+        },
+        cssModules: {
+          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+          config: {
+            namingPattern: 'module', // 转换模式，取值为 global/module
+            generateScopedName: '[name]__[local]___[hash:base64:5]',
+          },
+        },
+      },
+      webpackChain(chain) {
+        chain.resolve.plugin('tsconfig-paths').use(TsconfigPathsPlugin)
+        chain.plugin('unplugin-vue-components').use(Components({
+          resolvers: [NutUIResolver({ taro: true })],
+        }))
+      },
+    },
+    rn: {
+      appName: 'taroDemo',
+      postcss: {
+        cssModules: {
+          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        },
+      },
+    },
+  }
+  if (process.env.NODE_ENV === 'development') {
+    // 本地开发构建配置（不混淆压缩）
+    return merge({}, baseConfig, devConfig)
+  }
+  // 生产构建配置（默认开启压缩混淆等）
+  return merge({}, baseConfig, prodConfig)
+})
---
+++ mini-program/src/pages/index/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="index">
+    <nut-button type="primary" @click="onClick">
+      按一下按钮
+    </nut-button>
+    <nut-toast v-model:visible="show" msg="按钮被成功按下了" />
+  </view>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const show = ref(false)
+function onClick() {
+  show.value = true
+}
+</script>
