b29a046ed9fbcb91a24e2c297602c437daa8840f
WIFI连接超时 <localaccount@qq.com>
2025-11-04 01:17:14 +0800

    feat(captcha): 扩展滑块验证码资源配置
    - 增加本地缓存大小从20到200
    - 移除对ClassPathResourceProvider的显式引用
    - 动态加载41组滑块模板图片
    - 自动注册25张背景图片资源
    - 使用循环批量添加验证码模板和背景图
    - 简化资源配置逻辑，提高维护性

--- backend/src/main/java/top/lhlnb/backend/config/CaptchaResourceConfig.java
+++ backend/src/main/java/top/lhlnb/backend/config/CaptchaResourceConfig.java
@@ -5,12 +5,9 @@ import cloud.tianai.captcha.resource.ResourceStore;
 import cloud.tianai.captcha.resource.common.model.dto.Resource;
 import cloud.tianai.captcha.resource.CrudResourceStore;
 import cloud.tianai.captcha.resource.common.model.dto.ResourceMap;
-import cloud.tianai.captcha.resource.impl.provider.ClassPathResourceProvider;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-
-import static cloud.tianai.captcha.common.constant.CommonConstant.DEFAULT_SLIDER_IMAGE_TEMPLATE_PATH;
 
 /**
  * @author WIFI连接超时
@@ -27,21 +24,19 @@ public class CaptchaResourceConfig {
     public void init() {
         CrudResourceStore store = (CrudResourceStore) resourceStore;
 
-        // 滑块验证码 模板 (系统内置) ,这里添加的模板等同于  captcha.init-default-resource=true , 如果配置中设置了加载默认模板，这里模板可不用配置
-        ResourceMap template1 = new ResourceMap("default", 4);
-        template1.put("active.png", new Resource("classpath", "captcha/mask/active_1.png", "default"));
-        template1.put("fixed.png", new Resource("classpath", "captcha/mask/fixed_1.png", "default"));
+        // 配置滑块底图和滑块的源文件(png)(110x110)
+        int totalMaskImages = 41;
+        for (int i = 1; i <= totalMaskImages; i++) {
+            ResourceMap template = new ResourceMap("default", 4);
+            template.put("active.png", new Resource("classpath", "captcha/mask/" + i + ".png", "default"));
+            template.put("fixed.png", new Resource("classpath", "captcha/mask/" + i + "_bg.png", "default"));
+            store.addTemplate(CaptchaTypeConstant.SLIDER, template);
+        }
 
-        // 添加模板
-        store.addTemplate(CaptchaTypeConstant.SLIDER, template1);
-
-        // 添加自定义背景图片, resource 的参数1为资源类型(默认支持 classpath/file/url ), resource 的参数2为资源路径, resource 的参数3为标签
-        store.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "captcha/1.jpg", "default"));
-        store.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "captcha/2.jpg", "default"));
-        store.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "captcha/3.jpg", "default"));
-        store.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "captcha/4.jpg", "default"));
-        store.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "captcha/5.jpg", "default"));
-        store.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "captcha/6.jpg", "default"));
-        store.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "captcha/7.jpg", "default"));
+        // 配置背景图(jpg)(600x360)
+        int totalBgImages = 25;
+        for (int bgImg = 1; bgImg <= totalBgImages; bgImg++) {
+            store.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "captcha/" + bgImg + ".jpg", "default"));
+        }
     }
 }
